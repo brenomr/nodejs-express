13a6868951c817e863ea4d355eed730a0b9839f3
Breno Rodrigues <brenocartoon@hotmail.com>
2021-01-19 23:32:01 -0300

    Material atualizado

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 // Chamar a configuração do modulo customExpress em config
 const customExpress = require('./config/customExpress');
 
-// Inicializar o aplicativo
-const app = customExpress();
+const conexao = require('./infraestrutura/conexao');
+const tabelas = require('./infraestrutura/tabelas');
 
-// Configuração de porta e inicialização do servidor com mensagem de retorno
-app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
+const Tabelas = require('./infraestrutura/tabelas');
+
+conexao.connect((erro) => {
+    if (erro) {
+        console.log(erro);
+    }
+
+    else {
+        // Inicializar o aplicativo
+        const app = customExpress();
+
+        Tabelas.init(conexao);
+
+        // Configuração de porta e inicialização do servidor com mensagem de retorno
+        app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
+    }
+});
